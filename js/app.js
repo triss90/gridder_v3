e866d0e9fb9984f3742f36b2c74f04e766283c02
Tristan White <oliver.tristan@gmail.com>
2016-01-02 08:12:50 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,73 @@
+
+var MyApp = angular.module('MyApp', ['ngRoute', 'chieffancypants.loadingBar', 'ngAnimate']);
+
+// Defining URLs
+MyApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'pages/grid.html',
+      controller: 'MainCtrl'
+    })
+    .when('/colors', {
+      templateUrl: 'pages/colors.html',
+      controller: 'ColorsCtrl'
+    })
+    .when('/ux', {
+      templateUrl: 'pages/ux.html',
+      controller: 'UX'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+}]);
+
+// Main Controller
+MyApp.controller('MainCtrl', function ($scope, $http, $timeout, cfpLoadingBar) {
+  $scope.start = function() {
+    cfpLoadingBar.start();
+  };
+  $scope.complete = function() {
+    cfpLoadingBar.complete();
+  };
+  $scope.start();
+  $scope.fakeIntro = true;
+  $timeout(function() {
+    $scope.complete();
+    $scope.fakeIntro = false;
+  }, 750);
+});
+
+// Color Controller
+MyApp.controller('ColorsCtrl', function ($scope, $http, $timeout, cfpLoadingBar) {
+  $scope.start = function() {
+    cfpLoadingBar.start();
+  };
+  $scope.complete = function() {
+    cfpLoadingBar.complete();
+  };
+  $scope.start();
+  $scope.fakeIntro = true;
+  $timeout(function() {
+    $scope.complete();
+    $scope.fakeIntro = false;
+  }, 750);
+});
+
+// UX Controller
+MyApp.controller('UX', function ($scope, $http, $timeout, cfpLoadingBar) {
+  $scope.start = function() {
+    cfpLoadingBar.start();
+  };
+  $scope.complete = function() {
+    cfpLoadingBar.complete();
+  };
+  $scope.start();
+  $scope.fakeIntro = true;
+  $timeout(function() {
+    $scope.complete();
+    $scope.fakeIntro = false;
+  }, 750);
+});
+
+
+
